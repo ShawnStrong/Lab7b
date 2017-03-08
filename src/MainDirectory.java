06adb91d7b44e6db7ead2b7b9b9d0e9318705fd8
spstrong <shawnpstrong@gmail.com>
2017-03-08 14:47:42 -0600

    Interface Implementation

--- src/MainDirectory.java
+++ src/MainDirectory.java
@@ -5,18 +5,31 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
 
-public class MainDirectory {
+public class MainDirectory implements Directory{
 	LinkedList<Employee> sort = new LinkedList<>();
+	
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
 	void reconstruct(String s){
 		Gson g = new Gson();
 		ArrayList<Employee> ep = (g.fromJson(s, new TypeToken<Collection<Employee>>(){}.getType()));
 		for (Employee pp: ep) {
-			
+			pp.print();
 		}
 
-	}
-	public ArrayList<Employee> print(){
-		return null;
-		
 	}
 }
